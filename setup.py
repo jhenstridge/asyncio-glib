b63b11b92369fe77cd31f99a74413ab17c716f41
James Henstridge <james@jamesh.id.au>
2019-08-22 17:09:24 +0300

    setup: update setuptools build

--- setup.py
+++ setup.py
@@ -2,17 +2,27 @@
 
 from setuptools import setup
 
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
 setup(
     name="asyncio-glib",
     version="0.1",
-    description="GLib event loop integration for asyncio",
     author="James Henstridge",
     author_email="james@jamesh.id.au",
+    description="GLib event loop integration for asyncio",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/jhenstridge/asyncio-glib",
     packages=[
         "asyncio_glib",
     ],
-    license="LGPL-2+",
+    license="LGPL-2.1+",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
+        "Operating System :: POSIX",
+    ],
     install_requires=["PyGObject"],
     test_suite="tests",
 )
