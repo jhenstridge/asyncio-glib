41d59f90016ec9171c1b087cf1d0ec39a85a5ea7
chrysn <chrysn@fsfe.org>
2020-06-26 18:09:15 +0200

    events: Use own Futures class, let them break the loop

--- asyncio_glib/glib_events.py
+++ asyncio_glib/glib_events.py
@@ -26,6 +26,12 @@ class GLibEventLoop(asyncio.SelectorEventLoop):
         self._selector._main_loop.quit()
         return result
 
+    def create_future(self):
+        # Just taking the regular Future here and setting an add_done_callback
+        # would not cut it: Those done callbacks are executed from the Python
+        # main loop and not when the result is set, thus we'd be waiting for
+        # the very event we want to cause.
+        return GLibFuture(loop=self)
 
 class GLibEventLoopPolicy(asyncio.DefaultEventLoopPolicy):
     """An asyncio event loop policy that runs the GLib main loop"""
@@ -35,3 +41,14 @@ class GLibEventLoopPolicy(asyncio.DefaultEventLoopPolicy):
             raise RuntimeError("GLibEventLoopPolicy only allows the main "
                                "thread to create event loops")
         return GLibEventLoop()
+
+class GLibFuture(asyncio.Future):
+    __slots__ = []
+
+    def set_result(self, *args, **kwargs):
+        self.get_loop()._selector._main_loop.quit()
+        return super().set_result(*args, **kwargs)
+
+    def set_exception(self, *args, **kwargs):
+        self.get_loop()._selector._main_loop.quit()
+        return super().set_exception(*args, **kwargs)
